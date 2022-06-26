c1d5a8517be38b717293989e9ad393e59782273d
Neha0512 <78rakes@gmail.com>
2022-06-26 13:14:45 +0530

    Updated Carousel

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 
-let line2= document.querySelector('.line2');
-var x=0;
-window.onload=function(){
-    var line1= document.getElementById("line1");
-    setInterval(function(){
-        x++;
-        line1.style.left=x;
-    },10);
+const delay = 3000; //ms
+
+const slides = document.querySelector(".slides");
+const slidesCount = slides.childElementCount;
+const maxLeft = (slidesCount - 1) * 100 * -1;
+
+let current = 0;
+
+function changeSlide(next = true) {
+  if (next) {
+    current += current > maxLeft ? -100 : current * -1;
+  } else {
+    current = current < 0 ? current + 100 : maxLeft;
+  }
+
+  slides.style.left = current + "%";
 }
+
+let autoChange = setInterval(changeSlide, delay);
+const restart = function() {
+  clearInterval(autoChange);
+  autoChange = setInterval(changeSlide, delay);
+};
+
+// Controls
+document.querySelector(".next-slide").addEventListener("click", function() {
+  changeSlide();
+  restart();
+});
+
+document.querySelector(".prev-slide").addEventListener("click", function() {
+  changeSlide(false);
+  restart();
+});
